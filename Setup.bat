c39825e2dfa0eeb4b5860a47109d300b606a9aa1
swastikaspammer-hue <swastikaspammer@gmail.com>
2026-07-04 03:43:58 -0700

    Restructured repository directory layout to use bin subfolder

--- Setup.bat
+++ Setup.bat
@@ -3,10 +3,10 @@ title Madrilla Launcher Setup
 echo Setting up Madrilla Launcher...
 
 :: Create Start Menu Shortcut
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$WshShell = New-Object -ComObject WScript.Shell; $StartMenu = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs'); $Shortcut = $WshShell.CreateShortcut(\"$StartMenu\Madrilla Launcher.lnk\"); $Shortcut.TargetPath = \"$PSScriptRoot\MadrillaLauncher.exe\"; $Shortcut.WorkingDirectory = \"$PSScriptRoot\"; $Shortcut.Save()"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$WshShell = New-Object -ComObject WScript.Shell; $StartMenu = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs'); $Shortcut = $WshShell.CreateShortcut(\"$StartMenu\Madrilla Launcher.lnk\"); $Shortcut.TargetPath = \"$PSScriptRoot\bin\MadrillaLauncher.exe\"; $Shortcut.WorkingDirectory = \"$PSScriptRoot\bin\"; $Shortcut.Save()"
 
 :: Create Desktop Shortcut
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut(\"$Home\Desktop\Madrilla Launcher.lnk\"); $Shortcut.TargetPath = \"$PSScriptRoot\MadrillaLauncher.exe\"; $Shortcut.WorkingDirectory = \"$PSScriptRoot\"; $Shortcut.Save()"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut(\"$Home\Desktop\Madrilla Launcher.lnk\"); $Shortcut.TargetPath = \"$PSScriptRoot\bin\MadrillaLauncher.exe\"; $Shortcut.WorkingDirectory = \"$PSScriptRoot\bin\"; $Shortcut.Save()"
 
 echo.
 echo ===================================================
