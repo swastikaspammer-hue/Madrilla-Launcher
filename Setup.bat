f624b7ba752c0ce67e2efd562f9a39fc87a9d9d1
swastikaspammer-hue <swastikaspammer@gmail.com>
2026-07-04 03:40:15 -0700

    Added Setup.bat for shortcut creation

---
+++ Setup.bat
@@ -0,0 +1,17 @@
+@echo off
+title Madrilla Launcher Setup
+echo Setting up Madrilla Launcher...
+
+:: Create Start Menu Shortcut
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$WshShell = New-Object -ComObject WScript.Shell; $StartMenu = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs'); $Shortcut = $WshShell.CreateShortcut(\"$StartMenu\Madrilla Launcher.lnk\"); $Shortcut.TargetPath = \"$PSScriptRoot\MadrillaLauncher.exe\"; $Shortcut.WorkingDirectory = \"$PSScriptRoot\"; $Shortcut.Save()"
+
+:: Create Desktop Shortcut
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut(\"$Home\Desktop\Madrilla Launcher.lnk\"); $Shortcut.TargetPath = \"$PSScriptRoot\MadrillaLauncher.exe\"; $Shortcut.WorkingDirectory = \"$PSScriptRoot\"; $Shortcut.Save()"
+
+echo.
+echo ===================================================
+echo Setup complete! 
+echo Shortcuts created on your Desktop and Start Menu.
+echo ===================================================
+echo.
+pause
